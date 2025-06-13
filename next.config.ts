24109d541214e627d55501a5d4d2071238f3fbe6
3tared <mohamed2011305977@gmail.com>
2025-06-13 23:11:54 +0300

    `Added eslint configuration to next.config.ts`

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["j8v6vnsfxb.ufs.sh", "lh3.googleusercontent.com"],
   },
+  eslint: {
+    ignoreDuringBuilds: false,
+    dirs: ["pages", "components", "lib", "src"], // Only lint specific directories
+  },
 };
 
 export default nextConfig;
